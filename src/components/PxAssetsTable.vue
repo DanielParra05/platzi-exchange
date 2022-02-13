5a544182017d80698fe37a2be862949efb4c83d7
Daniel Parra <jose-paper@hotmail.com>
2022-02-13 00:32:53 -0500

    Adicion de sorting y searching

--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -3,19 +3,29 @@
     <thead>
       <tr class="bg-gray-100 border-b-2 border-gray-400">
         <th></th>
-        <th>
-          <span>Ranking</span>
+        <th :class="{ up: this.sortOrder == 1, down: this.sortOrder == -1 }">
+          <span class="underline cursor-pointer" @click="changeSortOrder"
+            >Ranking</span
+          >
         </th>
         <th>Nombre</th>
         <th>Precio</th>
         <th>Cap. de Mercado</th>
         <th>Variación 24hs</th>
-        <td class="hidden sm:block"></td>
+        <td class="hidden sm:block">
+          <input
+            class="bg-gray-100 focus:outline-none border-b border-gray-400 py-2 px-4 block w-full appearance-none leading-normal"
+            id="filter"
+            placeholder="Buscar..."
+            type="text"
+            v-model="filter"
+          />
+        </td>
       </tr>
     </thead>
     <tbody>
       <tr
-        v-for="coin in assets"
+        v-for="coin in filteredAssets"
         :key="coin.id"
         class="border-b border-gray-200 hover:bg-gray-100 hover:bg-orange-100"
       >
@@ -68,6 +78,12 @@ import { dollarFilter, percentFilter } from "@/filters";
 export default {
   name: "PxAssetsTable",
   components: { PxButton },
+  data() {
+    return {
+      filter: "",
+      sortOrder: 1,
+    };
+  },
   props: {
     assets: {
       type: Array,
@@ -80,6 +96,28 @@ export default {
     percentFilter,
     goToCoin(idCoin) {
       this.$router.push({ name: "coin-detail", params: { id: idCoin } });
+    },
+    changeSortOrder() {
+      this.sortOrder = this.sortOrder == 1 ? -1 : 1;
+    },
+  },
+
+  computed: {
+    filteredAssets() {
+      const altOrder = this.setOrder == 1 ? -1 : 1;
+
+      return this.assets
+        .filter(
+          (asset) =>
+            asset.symbol.toLowerCase().includes(this.filter.toLowerCase()) ||
+            asset.name.toLowerCase().includes(this.filter.toLowerCase())
+        )
+        .sort((a, b) => {
+          if (parseInt(a.rank) > parseInt(b.rank)) {
+            return this.sortOrder;
+          }
+          return altOrder;
+        });
     },
   },
 };
--- src/components/PxButton.vue
+++ src/components/PxButton.vue
@@ -3,7 +3,7 @@
     @click="buttonClick"
     class="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-2 border border-green-500 hover:border-transparent rounded"
   >
-   <pulse-loader :loading="isLoading"></pulse-loader>
+    <pulse-loader :loading="isLoading"></pulse-loader>
     <slot v-if="!isLoading"></slot>
   </button>
 </template>
@@ -12,11 +12,11 @@
 export default {
   name: "PxButton",
 
-  props:{
-    isLoading:{
-      type:Boolean,
-      default: false
-    }
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
   },
 
   methods: {
